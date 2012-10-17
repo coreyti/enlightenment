12e131f8f369cd7659fcda3ace0f562556d9bc60
Corey Innis <corey@coolerator.net>
2012-10-17 04:54:12 -0700

    checkpoint: bi-directional helpers

--- app/helpers/enlightenment/application_helper.rb
+++ app/helpers/enlightenment/application_helper.rb
@@ -1,4 +1,11 @@
 module Enlightenment
   module ApplicationHelper
+    def method_missing(method, *args, &block)
+      enlightenment.send(method, *args, &block)
+    rescue NoMethodError
+      main_app.send(method, *args, &block)
+    rescue NoMethodError
+      super
+    end
   end
 end
